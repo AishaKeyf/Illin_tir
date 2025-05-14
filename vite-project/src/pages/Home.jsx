7d793b3963996a72a18d5c7ca4e5aa5d89cef813
Aisha (sarajohn786) <bilandaahir77@gmail.com>
2025-05-14 08:42:31 +0300

    Restyling the website

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -15,13 +15,13 @@ const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     // slidesToScroll: 2,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 infinite: true,
                 dots: true,
